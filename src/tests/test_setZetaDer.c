87b4c988a77b53dad42fbf62aeb8e2d0874a24b3
Jonathan Busse <jonathan.busse@dlr.de>
2025-07-10 12:33:01 +0200

    Feature: 1D analytic set zeta derivatives test for x = 0

--- src/tests/test_setZetaDer.c
+++ src/tests/test_setZetaDer.c
@@ -20,6 +20,122 @@
 #endif
 
 /*!
+ * @brief Benchmarks 1D setZetaDer function by comparing to high-precision values
+ * from mathematica analytic implementation.
+ *
+ * @return number of failed tests.
+ * */
+int test_setZetaDer1D(void) {
+    printf("%s ", __func__);
+    char path[MAX_PATH_LENGTH];
+    int result = snprintf(path, sizeof(path), "%s/setZetaDer1D_Ref.csv", // NOLINT
+                          BASE_PATH);
+    if (result < 0 || result >= sizeof(path)) {
+        return fprintf(stderr, "Error creating file path\n");
+    }
+    FILE *data = fopen(path, "r");
+    if (data == NULL) {
+        return fprintf(stderr, "Error opening file: %s\n", path);
+    }
+
+    double nu;
+    double errorAbs;
+    double errorRel;
+    double errorMaxAbsRel;
+    double complex num;
+    double complex ref;
+    int scanResult;
+    char line[256];
+
+    int testsPassed = 0;
+    int totalTests = 0;
+    unsigned int dim = 1;
+    double tol = pow(10, -12);
+
+    double errMin = NAN;
+    double errMax = NAN;
+    double errSum = 0.;
+
+    double *nuRef = malloc(sizeof(double));
+    double a[] = {1.};
+    double x[] = {0.};
+    double *y = malloc(dim * sizeof(double));
+    unsigned int *alpha = malloc(dim * sizeof(unsigned int));
+    double *refRead = malloc(2 * sizeof(double));
+
+    printf("\n\t ... ");
+    printf("processing %s ", path);
+    while (fgets(line, sizeof(line), data) != NULL) {
+        // Scan: nu, y, alpha, {Re[result], Im[result]}
+        scanResult = sscanf( // NOLINT
+            line, "%lf,%lf,%u,%lf,%lf", nuRef, y, alpha, refRead, refRead + 1);
+
+        if (scanResult != 5) {
+            printf("\n\t ");
+            printf("Error reading line: %s", line);
+            printf("\t ");
+            printf("Scanned %d values instead of 5", scanResult);
+            continue;
+        }
+
+        nu = nuRef[0];
+
+        num = setZetaDer(nu, dim, a, x, y, alpha);
+        ref = refRead[0] + refRead[1] * I;
+
+        errorAbs = errAbs(ref, num);
+        errorRel = errRel(ref, num);
+
+        errorMaxAbsRel = (errorAbs < errorRel) ? errorAbs : errorRel;
+
+        errMin = (errMin < errorMaxAbsRel) ? errMin : errorMaxAbsRel;
+        errMax = (errMax > errorMaxAbsRel) ? errMax : errorMaxAbsRel;
+        errSum += errorMaxAbsRel;
+
+        if (errorMaxAbsRel < tol) {
+            testsPassed++;
+        } else {
+            printf("\n\n");
+            printf("Warning! ");
+            printf("setZetaDer: ");
+            printf(" %0*.16lf %+.16lf I (this implementation) \n\t\t!= "
+                   "%.16lf "
+                   "%+.16lf I (reference implementation)\n",
+                   4, creal(num), cimag(num), creal(ref), cimag(ref));
+            printf("Min(Emax, Erel):      %E !< %E  (tolerance)\n", errorMaxAbsRel,
+                   tol);
+            printf("\n");
+            printf("nu:\t\t %.16lf\n", nu);
+            printMatrixUnitTest("a:", a, dim);
+            printVectorUnitTest("x:\t\t", x, dim);
+            printVectorUnitTest("y:\t\t", y, dim);
+            printMultiindexUnitTest("alpha:\t\t", alpha, dim);
+            printf("\n");
+        }
+        totalTests++;
+    }
+
+    free(nuRef);
+    free(y);
+    free(alpha);
+    free(refRead);
+
+    if (fclose(data) != 0) {
+        return fprintf(stderr, "Error closing file: %d", errno);
+    }
+
+    printf("\n\t ... ");
+    printf("%d out of %d tests passed with tolerance %E.", testsPassed, totalTests,
+           tol);
+    printf("\t    ");
+    printf("[ Error →  min: %E | max: %E | avg: %E ]", errMin, errMax,
+           errSum / totalTests);
+    printf("\n");
+
+    return totalTests - testsPassed;
+}
+
+/*!
  * @brief Benchmarks 2D setZetaDer function by comparing to high-precision values
  * from mathematica prototype over a range of random parameters.
  *
@@ -61,7 +177,7 @@ int test_setZetaDer_prototype(void) {
     double *x = malloc(dim * sizeof(double));
     double *y = malloc(dim * sizeof(double));
     unsigned int *alpha = malloc(dim * sizeof(unsigned int));
-    double *refRead = malloc(dim * sizeof(double));
+    double *refRead = malloc(2 * sizeof(double));
 
     printf("\n\t ... ");
     printf("processing %s ", path);
@@ -146,7 +262,7 @@ int test_setZetaDer_prototype(void) {
  * @return number of failed tests.
  */
 int test_setZetaDer_taylor(void) { // NOLINT
-    printf("%s ... ", __func__);
+    printf("%s ", __func__);
     double errorAbs;
     double errorRel;
     double errorMaxAbsRel;
@@ -267,7 +383,8 @@ int test_setZetaDer_taylor(void) { // NOLINT
  * @return number of failed tests.
  */
 int main() {
-    int failed1 = test_setZetaDer_prototype();
-    int failed2 = test_setZetaDer_taylor();
-    return failed1 + failed2;
+    int failed1 = test_setZetaDer1D();
+    int failed2 = test_setZetaDer_prototype();
+    int failed3 = test_setZetaDer_taylor();
+    return failed1 + failed2 + failed3;
 }
