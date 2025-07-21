50f0088db000df1ef73d52c60539a2504f13a109
Jonathan Busse <jonathan.busse@dlr.de>
2025-07-21 15:52:49 +0200

    Feature: Mathematica wrapper

--- mathematica/zeta_mathematica.h
+++ mathematica/zeta_mathematica.h
@@ -57,4 +57,22 @@ int epstein_zeta_reg_mathematica_call(double *out, double nu, int dim,
 int set_zeta_der_mathematica_call(double *out, double nu, int dim, const double *a,
                                   const double *x, const double *y,
                                   const unsigned int *alpha);
+
+/*
+ * @brief Saves real and imaginary parts of the derivatives of the regularized
+ * Epstein zeta function in an array.
+ * @param[in, out] out: 2D array, where real and imaginary parts of the regularized
+ * Epstein zeta function derivatives will be stored.
+ * @param[in] nu: exponent for the Epstein zeta function.
+ * @param[in] dim: dimension of the input vectors.
+ * @param[in] a: matrix that transforms the lattice in the Epstein zeta
+ * function.
+ * @param[in] x: x vector of the Epstein zeta function.
+ * @param[in] y: y vector of the Epstein zeta function.
+ *  @return 0
+ */
+int epstein_zeta_reg_der_mathematica_call(double *out, double nu, int dim,
+                                          const double *a, const double *x,
+                                          const double *y,
+                                          const unsigned int *alpha);
 #endif
