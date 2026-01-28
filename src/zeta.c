3fc67ba30fc94d9d12d276e59472557112ae40e0
Jonathan Busse <jonathan.busse@dlr.de>
2026-01-28 17:29:21 +0100

    Fix: hardcode der = 0 in nu = 0

--- src/zeta.c
+++ src/zeta.c
@@ -739,8 +739,8 @@ double complex epsteinZetaInternal(double nu, unsigned int dim, // NOLINT
     // handle special case of non-positive integer values nu.
     double complex res = 0.;
     double y_t2_squared = dot(dim, y_t2, y_t2);
-    if (variant < 2 && nu < 1 && fabs((nu / 2.) - nearbyint(nu / 2.)) < EPS) {
-        if (dot(dim, x_t2, x_t2) == 0 && nu == 0) {
+    if (variant < 3 && nu < 1 && fabs((nu / 2.) - nearbyint(nu / 2.)) < EPS) {
+        if (variant < 2 && dot(dim, x_t2, x_t2) == 0 && nu == 0) {
             res = -1 * cexp(-2 * M_PI * I * dot(dim, x_t1, y_t2));
         } else {
             res = 0;
