afc27ece6a38370b831f5dcd5f41627b7619b52a
Jonathan Busse <jonathan.busse@dlr.de>
2026-04-17 13:57:34 +0200

    Refactor: sum_real_harmonic_1D gets summand func

--- src/zeta.c
+++ src/zeta.c
@@ -226,7 +226,7 @@ static double complex sum_real_der(double nu, unsigned int dim, double lambda,
 }
 
 /**
- * @brief Computes one summand of the derivative of the first sum in Crandall's
+ * @brief Computes one summand of the harmonic method of the first sum in Crandall's
  * formula.
  * @param[in] nu: exponent for the Epstein zeta function.
  * @param[in] kIndex: specifies degree |alpha| - 2k.
@@ -548,8 +548,36 @@ static double complex sum_real_harmonic_large_exp_singularity_sum( // NOLINT
 }
 
 /**
+ * @brief Computes one summand of the harmonic method of the first sum in Crandall's
+ * formula in 1D.
+ * @param[in] nu: exponent for the Epstein zeta function.
+ * @param[in] dim: dimension of the input vectors.
+ * @param[in] lambda: parameter that decides the weight of each sum.
+ * @param[in,out] lv: lattice vector, shifted by x in-place.
+ * @param[in] x: projection of x vector to elementary lattice cell.
+ * @param[in] y: projection of y vector to elementary lattice cell.
+ * @param[in] zArgBound: global bound on when to use the asymptotic expansion in
+ * the incomplete gamma evaluation.
+ * @param[in] alphaAbs: total of alpha.
+ * @return h₍α,kIndex₎(y) * G_{nu}(z - x) * exp(-2 * PI * I * (z * y), the real space
+ * summand of the harmonic method.
+ */
+static inline double complex summand_real_harmonic_1D(
+    double nu, unsigned int dim, double lambda, double lv[], const double *x,
+    const double *y, double zArgBound, unsigned int alphaAbs) {
+
+    double complex rot = cexp(-2 * M_PI * I * dot(dim, lv, y));
+    for (int i = 0; i < dim; i++) {
+        lv[i] = lv[i] - x[i];
+    }
+    double h = harmonic_h_1D_kMax(lv, alphaAbs);
+
+    return rot * h * crandall_g(dim, nu, lv, 1. / lambda, zArgBound);
+}
+
+/**
  * @brief calculates the an harmonic polynomial applied to the summands in real
- * space.
+ * space in 1D.
  * @param[in] nu: exponent for the Epstein zeta function.
  * @param[in] dim: dimension of the input vectors.
  * @param[in] m: matrix that transforms the lattice in the Epstein zeta
@@ -570,45 +598,28 @@ static double complex sum_real_harmonic_1D(double nu, unsigned int dim,
                                            const double *y, const int cutoffs[],
                                            double zArgBound, bool diag,
                                            unsigned int alphaAbs) {
-
     double lambda = 1.; // parameter that decides the weight of each sum
 
     int zv[dim];    // counting vector in Z^dim
     double lv[dim]; // lattice vector
     // cuboid cutoffs
     long totalSummands = 1;
-    long totalCutoffs[dim + 1];
     for (int k = 0; k < dim; k++) {
-        totalCutoffs[k] = totalSummands;
+        zv[k] = -cutoffs[k]; // lattice vector initialized
         totalSummands *= 2 * cutoffs[k] + 1;
     }
     double complex sum = 0.0;
     double complex epsilon = 0.0;
-    double complex auxt;
-    double complex auxy;
-    double complex rot;
-
-    double h;
 
     // First Sum (in real space)
     for (long n = 0; n < totalSummands; n++) {
-        for (int k = 0; k < dim; k++) {
-            zv[k] =
-                (((int)(n / totalCutoffs[k])) % (2 * cutoffs[k] + 1)) - cutoffs[k];
-        }
         matrix_intVector(dim, m, zv, lv, diag);
-        rot = cexp(-2 * M_PI * I * dot(dim, lv, y));
-        for (int i = 0; i < dim; i++) {
-            lv[i] = lv[i] - x[i];
-        }
-
-        h = harmonic_h_1D_kMax(lv, alphaAbs);
-
-        auxy = rot * h * crandall_g(dim, nu, lv, 1. / lambda, zArgBound) - epsilon;
-        auxt = sum + auxy;
-        epsilon = (auxt - sum) - auxy;
-        sum = auxt;
+        double complex summand =
+            summand_real_harmonic_1D(nu, dim, lambda, lv, x, y, zArgBound, alphaAbs);
+        kahan_add(&sum, &epsilon, summand);
+        lattice_vector_increment(dim, cutoffs, zv);
     }
+
     return sum;
 }
 
