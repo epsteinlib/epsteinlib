debe35cfd3732c0df9d8a9bdd999575a5a2a5000
Jonathan Busse <jonathan.busse@dlr.de>
2026-07-28 13:51:43 +0200

    Fix: add cutoff near eval -1 since promised in readme

--- src/zeta.c
+++ src/zeta.c
@@ -261,7 +261,7 @@ double complex epsteinZetaInternal(double nu, unsigned int dim, // NOLINT
     // handle special case of non-positive integer values nu.
     double complex res;
     if (nu < 1 && fabs((nu / 2.) - nearbyint(nu / 2.)) < EPS) {
-        if (dot(dim, x_t2, x_t2) == 0 && nu == 0) {
+        if (dot(dim, x_t2, x_t2) <= EPS_ZERO_Y && nu == 0) {
             if (reg) {
                 res = -1; // reg already carries phase by definition
             } else {
